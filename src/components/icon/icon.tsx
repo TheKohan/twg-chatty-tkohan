a0628bb5c58a7757cd6cb5aa77ef64f9cf88541f
TheKohan <kohan142011@gmail.com>
2024-07-23 22:06:55 +0200

    feat: add icon component

---
+++ src/components/icon/icon.tsx
@@ -0,0 +1,21 @@
+import Phone from '../../../assets/icons/phone.svg';
+import Plus from '../../../assets/icons/plus.svg';
+import Profile from '../../../assets/icons/profile.svg';
+import Search from '../../../assets/icons/search.svg';
+import Send from '../../../assets/icons/send.svg';
+import Videocall from '../../../assets/icons/videocall.svg';
+import Vision from '../../../assets/icons/vision.svg';
+import VisionLow from '../../../assets/icons/vision-low.svg';
+import Rooms from '../../../assets/icons/rooms.svg';
+
+export const Icon = {
+  phone: Phone,
+  plus: Plus,
+  profile: Profile,
+  search: Search,
+  send: Send,
+  videocall: Videocall,
+  vision: Vision,
+  visionLow: VisionLow,
+  rooms: Rooms,
+} as const;
