2ed569fd9844db46d4eff76e21f0de1cd926986d
TheKohan <kohan142011@gmail.com>
2024-07-23 21:24:48 +0200

    feat: setup fonts, layout

---
+++ src/app/layout.tsx
@@ -0,0 +1,40 @@
+import { PropsWithChildren } from 'react';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { fontAssets } from '@chatty/theme';
+
+SplashScreen.preventAutoHideAsync();
+
+export const Layout: React.FC<PropsWithChildren> = ({ children }) => {
+  const [loaded, error] = useFonts(fontAssets);
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={{ fontFamily: 'Inter-Black', fontSize: 30 }}>
+        Inter Black
+      </Text>
+      <Text style={{ fontSize: 30 }}>Platform Default</Text>
+      <View style={{ flex: 1 }}>{children}</View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export * from './app';
+export * from './theme';
+export * from './pages';
+export * from './components';
