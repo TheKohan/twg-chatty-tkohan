2c6358a47598d76ac31c8541e3b404805b21ab4c
TheKohan <kohan142011@gmail.com>
2024-07-24 14:10:41 +0200

    feat: added room composables

--- src/screens/room/room.tsx
+++ src/screens/room/room.tsx
@@ -1,18 +1,34 @@
 import { RoomNavigationProp, RoomRouteProp } from '@chatty/types';
-import { Button } from '@chatty/components';
 import { useNavigation, useRoute } from '@react-navigation/native';
-import { Text, View } from 'react-native';
-import { useCallback, useEffect, useState } from 'react';
-import { GiftedChat, IMessage } from 'react-native-gifted-chat';
+import { Dimensions, Text, View } from 'react-native';
+import { FC, useCallback, useState } from 'react';
+import {
+  Bubble,
+  BubbleProps,
+  GiftedChat,
+  IMessage,
+  InputToolbar,
+  InputToolbarProps,
+  Send,
+  SendProps,
+} from 'react-native-gifted-chat';
 import { useQuery } from '@apollo/client';
 import { GET_CURRENT_USER, GET_ROOM } from '@chatty/graphql';
 import { mapToGiftedMessages, mapUserToGifted } from '@chatty/utils';
+import { colors } from '@chatty/theme';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { Icon } from '@chatty/components';
+
+/**
+ * Theres an issue https://github.com/FaridSafi/react-native-gifted-chat/issues/2498
+ * with the GiftedChat compability with expo sdk 51, don't have time for now to dig into it, causes warnings
+ */
 
 export const Room = () => {
-  const navigation = useNavigation<RoomNavigationProp>();
   const { params } = useRoute<RoomRouteProp>();
   const { loading, error, data } = useQuery(GET_ROOM, {
     variables: { id: params.roomId },
+    pollInterval: 1000,
   });
   const { data: userData } = useQuery(GET_CURRENT_USER);
 
@@ -35,6 +51,7 @@ export const Room = () => {
   }
 
   const onSend = useCallback((messages: IMessage[] = []) => {
+    console.log(messages);
     setMessages(previousMessages =>
       GiftedChat.append(previousMessages, messages)
     );
@@ -47,20 +64,76 @@ export const Room = () => {
         inverted={false}
         user={data.room.user ? mapUserToGifted(userData?.user) : undefined}
         onSend={onSend}
-        renderBubble={props => <Text>{props.currentMessage?.text}</Text>}
+        renderBubble={props => <ChatBubble {...props} />}
         // isTyping={}
         // onInputTextChanged={}
         renderFooter={() => <Text>Footer</Text>}
-        renderInputToolbar={props => <Text>Text Input</Text>}
+        renderInputToolbar={props => <ChatInput {...props} />}
         alwaysShowSend
-        renderSend={props => <Text>SEND</Text>}
+        renderSend={props => <ChatSend {...props} />}
         keyboardShouldPersistTaps='always'
       />
-      <Button
-        variant='primary'
-        label='Go to rooms'
-        onPress={() => navigation.navigate('Rooms')}
-      />
     </View>
+  );
+};
+
+const ChatSend: FC<SendProps<IMessage>> = props => {
+  return (
+    <Send
+      {...props}
+      containerStyle={{
+        position: 'absolute',
+        right: -50,
+        top: 0,
+      }}
+    >
+      <Icon.send />
+    </Send>
+  );
+};
+
+const ChatInput: FC<InputToolbarProps<IMessage>> = props => {
+  const insets = useSafeAreaInsets();
+
+  return (
+    <InputToolbar
+      {...props}
+      containerStyle={{
+        borderRadius: 12,
+        borderBottomRightRadius: 0,
+        // width: Layout.window.width-80,
+        backgroundColor: colors.blue300,
+        padding: 12,
+        paddingBottom: insets.bottom,
+      }}
+      primaryStyle={{
+        backgroundColor: colors.white,
+        width: Dimensions.get('window').width - 80,
+      }}
+    />
+  );
+};
+
+const ChatBubble: FC<Readonly<BubbleProps<IMessage>>> = props => {
+  return (
+    <Bubble
+      {...props}
+      textStyle={{
+        left: { color: colors.black },
+        right: { color: colors.white },
+      }}
+      renderTime={() => null}
+      wrapperStyle={{
+        left: {
+          padding: 4,
+          backgroundColor: colors.white,
+          borderBottomLeftRadius: 0,
+        },
+        right: {
+          backgroundColor: colors.plum300,
+          borderBottomRightRadius: 0,
+        },
+      }}
+    />
   );
 };
